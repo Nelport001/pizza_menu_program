aab2eb3339b55e5cb0b3bd1120cd0e60146383fc
Nelport001 <nelport001@gmail.com>
2025-12-09 11:15:30 -0500

    Delete polling.py

--- polling.py
+++
@@ -1,5 +0,0 @@
-poll_names = ['jen', 'sarah', 'edward', 'phil', 'aaron']
-for name in poll_names:
-    print(f"Thank you {name} for taking the poll")
-if 'daniel' not in poll_names:
-    print("daniel please take the poll")
