01564fac58ebcfb34666ee7ff6e9b532a6a645de
Nelport001 <nelport001@gmail.com>
2025-12-09 11:18:09 -0500

    Delete favorite_numbers.py

--- favorite_numbers.py
+++
@@ -1,7 +0,0 @@
-favorite_numbers = {'Leon': [2, 3, 4],
-                    'fanny': [5, 6],
-                    'Ana': [8, 10, 16]}
-for name, number in favorite_numbers.items():
-    print(f"\n{name}'s favorite numbers are:")
-    for num in number:
-        print(f"{num}")
