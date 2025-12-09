12e10dec24257f5ac92248c0588033b34910b5c1
Nelport001 <nelport001@gmail.com>
2025-12-09 11:17:54 -0500

    Delete foods.py

--- foods.py
+++
@@ -1,7 +0,0 @@
-my_foods = ['pizza', 'falafel', 'carro cake']
-friends_foods = my_foods[:]
-for food in my_foods:
-    print(f"{food.title()} is one of my favorite foods")
-print("\n")
-for food in friends_foods:
-    print(f"{food.title()} is one of my friends favorite foods")
